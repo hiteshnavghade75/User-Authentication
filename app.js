4457f89441b485900fa597e558ee17f4a1b0f74c
hiteshnavghade75 <hiteshnavghade75@gmail.com>
2023-07-22 23:44:12 +0530

    added user and admin authentication

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express();
 const db = require('./db/connection')
-const userRouter = require('./routes/routes.user')
+const userRouter = require('./routes/routes.user');
+const adminRouter = require('./routes/routes.admin');
+const productRouter = require("./routes/routes.products")
 
 require('dotenv').config();
 const PORT = process.env.PORT;
@@ -12,6 +14,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
 
 app.use('/user', userRouter)
+app.use('/admin', adminRouter)
+app.use('/product', productRouter)
 
 app.use((req,res) => {
     res.send("Welcome to our api");
